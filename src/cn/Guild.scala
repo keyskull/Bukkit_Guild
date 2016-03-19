31926843500182352fb2db18897889a234de86f4
keyskull <a410615903@gmail.com>
2016-03-20 00:24:28 +0800

    完成基本接口星期一之后再开射~~

--- src/cn/Guild.scala
+++ src/cn/Guild.scala
@@ -6,17 +6,19 @@ package cn
 import java.util.logging.Level
 import org.bukkit.plugin.java.JavaPlugin
 import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 class Guild extends JavaPlugin{
-  lazy val config_handle= this.getConfig()
+  lazy val config_handle= this.getConfig
 
   override  def onLoad(): Unit ={
-    getLogger().info("onLoad has been invoked!")
+    getLogger.info("onLoad has been invoked!")
+    getLogger.log(Level.WARNING,"yo")
 
 
   }
   override def onEnable():Unit= {
-    getLogger().info("onEnable has been invoked!")
+    getLogger.info("onEnable has been invoked!")
 
     this.saveConfig()
     this.saveDefaultConfig()
@@ -25,7 +27,7 @@ class Guild extends JavaPlugin{
 
   }
   override def onDisable():Unit={
-    getLogger().info("onDisable has been invoked!")
+    getLogger.info("onDisable has been invoked!")
     getLogger.log(Level.WARNING,"yo")
   }
 }
@@ -36,7 +38,7 @@ object Guild{
   case object WARN extends Massge_type
   case object ERROR extends Massge_type
   import scala.collection.mutable.Map
-  lazy val config:Map[String, Object]=Map(
+  lazy val config:mutable.Map[String, Object]=mutable.Map(
     "Create_Guild_Money" -> "50000",
     "Guild-Max-People" -> "50"
   )
